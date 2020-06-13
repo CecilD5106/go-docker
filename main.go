a3277792dc7e5e6250859911860ef47dc571f88d
Cecil Davis <cgdavis@localhost.localdomain>
2020-06-13 10:21:40 -0500

    Initial commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func pong(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", pong)
+	r.Run(":8000")
+}
